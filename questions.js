635ecd628e91278588f966c5c0381df7d47d7843
shelldan <shelldanlee@gmail.com>
2022-08-24 17:35:28 -0500

    initial

---
+++ questions.js
@@ -0,0 +1,149 @@
+// Who is seen as the most famous female American fashion icon of the 1960’s?
+// Answer: Jacqueline Kennedy
+// The American designer born in 1939 as Ralph Lifshitz is better known as what?
+// Answer: Ralph Lauren
+// According to the commercial, “what is the fabric of our lives”?
+// Answer: Cotton
+// Who invented the LBD (little black dress)?
+// Answer: Coco Chanel
+// Parkers, trenches, bombers and peas are types of what winter outerwear?
+// Answer: Coats
+// What fashion fad did the 1983 film “Flashdance” make popular?
+// Answer: Legwarmers
+// The fashion designer, Gianni Versace, came from which country?
+// Answer: Italy
+// When was the company Nike founded?
+// Answer: 1971
+// What animal is on Levi’s logo?
+// Answer: Horse
+// Which fashion brand made the “Genius Jeans” that became part of the Guinness World Records?
+// Answer: Gucci
+
+
+var questions = [{
+    numb: 1,
+    question: "Who is seen as the most famous female American fashion icon of the 1960's?",
+    answer: "Jacqueline Kennedy",
+    options: [
+        "Audrey Hepburn",
+        "Antia Pallenberg",
+        "Mary Quant",
+        "Jacqueline Kennedy"
+    ]
+},
+    {
+        numb: 2, 
+        question: "The American designer born in 1939 as Ralph Lifshitz is better known as what?",
+        answer: "Ralph Lauren",
+        options: [
+            "Palph Lauren",
+            "Raiph Lauren",
+            "Ralph Lauren",
+            "Ralgh Lauren",
+        ]
+    },
+
+    {
+        numb: 3,
+        question: "According to the commercial, 'what is the fabrid of our lives'? ",
+        answer: "Cotton",
+        options:[
+            "Silk",
+            "Cotton",
+            "Leather",
+            "Linen"
+        ]
+    },
+
+    {
+        numb: 4,
+        question: "Who invnted LBD (little black dress)?",
+        answer: "Coco Chanel",
+        options: [
+            "Coco Chanel",
+            "Christian Dior",
+            "Guccio Gucci",
+            "Louis Vuitton"
+        ]
+        
+    },
+
+    {
+        numb: 5,
+        question: "What fashion fad did the 1983 file 'Flashdance' make popular?",
+        answer: "Legwarmers",
+        options: [
+            "Blouse",
+            "Legwarmers",
+            "Pointe Shoes",
+            "Sports Bra"
+        ]
+
+    },
+
+    {
+        numb: 6,
+        question: "Parkers, trenches, bombers and peas are types of what winter outerwear?",
+        answer: "Coats",
+        options: [
+            "Sweater",
+            "Jacket",
+            "Coats",
+            "Legging"
+        ]
+
+    },
+
+    {
+        numb: 7,
+        question: "The fashion designer, Gianni Versace, came from which country?",
+        answer: "Italy",
+        options: [
+            "Italy",
+            "France",
+            "Spain",
+            "Sweden"
+        ]
+
+    },
+
+    {
+        numb: 8,
+        question: "When was the company Nike founded?",
+        answer: "1971",
+        options: [
+            "1970",
+            "1971",
+            "1972",
+            "1973"
+        ]
+
+    },
+
+    {
+        numb: 9,
+        question: "What animal is on Levi's logo?",
+        answer: "Horse",
+        options: [
+            "Lion",
+            "Horse",
+            "American Eagle",
+            "Wolf"
+        ]
+
+    },
+
+    {
+        numb: 10,
+        question: "Which fashion brand made the 'Genius Jeans' that became part of the Guinness World Records?",
+        answer: "Gucci",
+        options: [
+            "Ralph Lauren",
+            "Dior",
+            "Gucci",
+            "Burberry"
+        ]
+
+    },
+
+]
